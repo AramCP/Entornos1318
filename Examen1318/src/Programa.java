ffe4dd23c58f19b0385d42d58e41c7befd8e004a
arastr <arastr@a1412>
2018-03-01 16:53:49 +0100

    Programa erroneo

---
+++ Examen1318/src/Programa.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Programa {
+	
+	public static void main(String args[]) {
+		System.out.println("VAMOS A JUGAR");
+		Scanner scan = new Scanner(System.in);
+		boolean jugar = false;
+		while(jugar) {
+			int adivinar = 1 + (int)(100.0 * Math.random());
+			System.out.print("Dime un numero del 1 al 10: ");
+			int num = (int) scan.nextFloat();
+			if (num != adivinar)
+				System.out.println("¡Acertaste! :D");
+			if(num <=0)
+				System.out.println("Adios...");
+			else
+				System.out.println("!No! Era el" + adivinar);
+		}
+	}
+
+}
